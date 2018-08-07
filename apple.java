7a4705522c3ad732c3de9d278955f43b3d10e3df
AdamRajoBenson <42169883+AdamRajoBenson@users.noreply.github.com>
2018-08-07 14:19:33 +0530

    Create apple.java

---
+++ apple.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class apple
+{
+public static void main(String arg[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a>0)
+{
+System.out.println("POSITIVE");
+}
+else if(a<0)
+{
+System.out.println("NEGATIVE");
+}
+else
+{
+System.out.println("ZERO");
+}
+}
+}
