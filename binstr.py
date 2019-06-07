0ffe562bfaf668373a45daa946df5b2dfb6ad173
AdamRajoBenson <42169883+AdamRajoBenson@users.noreply.github.com>
2019-06-07 16:28:32 +0530

    Update binstr.py

--- binstr.py
+++ binstr.py
@@ -1,6 +1,9 @@
 w=input()
-for i in range(0,len(w)):
-  if((i!=0)or(i!=1)):
-    print("no")
-else:
+y=0
+for i in w:
+  if((i=='0')or(i=='1')):
+    y=y+1
+if(y==len(w)):
   print("yes")
+else:
+  print("no")
